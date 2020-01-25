75130994854133c2cc7658005e746a194bf45892
Gaurab KC <kecy.gaurab@gmail.com>
2020-01-25 12:22:38 +0200

    configured eslint and added .env

---
+++ src/components/book.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+
+const Book = (books) => {
+    <Grid container></Grid>
+}
+
+export default Book
