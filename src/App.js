602030b8f6a488063f3e8b7e9145efc92376a654
Gaurab KC <35660893+kecyGaurab@users.noreply.github.com>
2020-01-26 22:52:23 +0200

    book component and search feature added (#2)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,81 @@
-import React from 'react'
-import NavBar from './components/navBar'
-import Books from './components/books'
+import React, {Fragment, useState, useEffect} from 'react';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+// import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import NavBar from './components/navBar';
+import Book from './components/book';
+import {CssBaseline} from '@material-ui/core';
 
 const App = () => {
-  return (
-    <div className="App">
-      <NavBar />
-    <Books/>
-    </div>
-  )
-}
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const [books, setbooks] = useState ([]);
+  const [query, setQuery] = useState ('');
+  const [searchParameter, setSearchParameter] = useState ('');
 
-export default App
+  const handleSearchParameter = e => {
+    setSearchParameter (e.target.value);
+  };
+
+  const handleSearch = e => {
+    setQuery (e.target.value);
+  };
+
+  function getByBookName (query, apiKey) {
+    return axios.get (
+      `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}&maxResults=40`
+    );
+  }
+
+  function getByAuthor (query, apiKey) {
+    return axios.get (
+      `https://www.googleapis.com/books/v1/volumes?q=''+inauthor:${query}&orderBy=newest&key=${apiKey}&maxResults=40`
+    );
+  }
+
+  useEffect (
+    () => {
+      searchParameter === 'Book-Name'
+        ? getByBookName (query, apiKey)
+        : getByAuthor (query, apiKey)
+            .then (res => {
+              const response = res.data;
+              console.log ('response', response);
+              setbooks (response.items);
+            })
+            .catch (err => {
+              console.log ('error is:', err);
+              setQuery ('a');
+            });
+    },
+    [query, searchParameter, apiKey]
+  );
+
+  return (
+    <Fragment>
+      <CssBaseline />
+      <NavBar
+        handleSearch={handleSearch}
+        handleSearchParameter={handleSearchParameter}
+      />
+
+      <Container style={{padding: '10px 0'}}>
+        <Grid container justify="space-around" direction="row" spacing={6}>
+          {books
+            ? books.map (book => (
+                <Fragment key={book.etag}>
+                  {book.volumeInfo.imageLinks
+                    ? <Grid key={book.etag} item xs={3}>
+                        <Book book={book} />
+                      </Grid>
+                    : null}
+                </Fragment>
+              ))
+            : <p> No results</p>}
+        </Grid>
+      </Container>
+    </Fragment>
+  );
+};
+
+export default App;
--- src/components/book.js
+++ src/components/book.js
@@ -1,9 +1,101 @@
-import React from 'react'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
+import React, {useState} from 'react';
+import {
+  Link,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Grid,
+  Typography,
+  Collapse,
+} from '@material-ui/core';
 
-const Book = (books) => {
-    <Grid container></Grid>
-}
+import IconButton from '@material-ui/core/IconButton';
+import styled from 'styled-components';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {StyledCard, StyledLink} from './styledCard';
 
-export default Book
+const Image = styled.div`
+height:180px;
+`;
+
+const Title = styled.div`
+height:40px;
+overflow:hidden;
+`;
+
+const Book = ({book}) => {
+  const [expanded, setExpanded] = useState (false);
+  const handleExpandClick = () => {
+    setExpanded (!expanded);
+  };
+
+  return (
+    <StyledCard extended={expanded} elevation={5}>
+      <CardHeader
+        title={
+          <Title>
+            <Typography variant="h6">
+              {book.volumeInfo.title}
+            </Typography>
+          </Title>
+        }
+        subheader={
+          book.volumeInfo.authors
+            ? book.volumeInfo.authors
+                .filter ((author, index) => [index] < 1)
+                .map (author => (
+                  <Typography key={author} variant="subtitle2">
+                    {author}
+                  </Typography>
+                ))
+            : null
+        }
+      />
+
+      <CardContent>
+        <Grid container direction="column" alignItems="center" spacing={5}>
+
+          <Grid item>
+            <Image>
+              <img
+                alt="book"
+                src={
+                  book.volumeInfo.imageLinks
+                    ? book.volumeInfo.imageLinks.thumbnail
+                    : null
+                }
+              />
+            </Image>
+          </Grid>
+        </Grid>
+      </CardContent>
+
+      <CardActions>
+        <IconButton onClick={() => handleExpandClick (book)}>
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+
+          <Typography variant="h6">
+            Description
+          </Typography>
+          <Typography variant="body2">
+            {book.searchInfo ? book.searchInfo.textSnippet : null}
+          </Typography>
+
+          <Link href={book.volumeInfo.previewLink}>
+            <StyledLink variant="body1">
+              Link to Book
+            </StyledLink>
+
+          </Link>
+
+        </CardContent>
+      </Collapse>
+    </StyledCard>
+  );
+};
+
+export default Book;
