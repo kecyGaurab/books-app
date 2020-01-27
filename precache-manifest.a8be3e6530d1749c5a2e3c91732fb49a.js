06a8a194dde3e2ed7361283e44d4136a91913743
Gaurab KC <kecy.gaurab@gmail.com>
2020-01-27 21:36:55 +0200

    Updates

---
+++ precache-manifest.a8be3e6530d1749c5a2e3c91732fb49a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2ef519b20bf3fc8a64a4e08b3c4083",
+    "url": "/books-app/public/index.html"
+  },
+  {
+    "revision": "14c08174f34765652c59",
+    "url": "/books-app/public/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "0bd8c2b6af9291a62478",
+    "url": "/books-app/public/static/js/2.1ad12ae8.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/books-app/public/static/js/2.1ad12ae8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14c08174f34765652c59",
+    "url": "/books-app/public/static/js/main.1bf09210.chunk.js"
+  },
+  {
+    "revision": "aa0da183d0770378c3ac",
+    "url": "/books-app/public/static/js/runtime-main.8617e669.js"
+  }
+]);
