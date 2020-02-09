e8b508ab97fc0f4fc839fb8cc1261002dcd70a7b
Gaurab KC <35660893+kecyGaurab@users.noreply.github.com>
2020-02-09 20:12:18 +0200

    Redux (#7)
    * package deleted
    
    * set up redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Book from '../src/components/book';
 import {ThemeProvider} from '@material-ui/core/styles';
 import theme from './theme';
+import {Provider} from 'react-redux';
+import {rootReducer} from '../src/reducers/rootReducer';
+
+import {createStore} from 'redux';
+
+const store = createStore (rootReducer);
 
 ReactDOM.render (
   <ThemeProvider theme={theme}>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </ThemeProvider>,
   document.getElementById ('root')
 );
